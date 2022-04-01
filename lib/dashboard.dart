b9064f658a2d12e24acafe723b73fd953920c5bf
Sven Dockx <sven.dockx@gmail.com>
2022-04-01 13:54:11 +0200

    input to send message

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -7,67 +7,59 @@ import 'detail.dart';
 import 'functions/FirestoreHelper.dart';
 import 'model/Utilisateur.dart';
 
-class dashBoard extends StatefulWidget{
+class dashBoard extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return dashBoardState();
   }
-
 }
 
-class dashBoardState extends State<dashBoard>{
+class dashBoardState extends State<dashBoard> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       drawer: myDrawer(),
       appBar: AppBar(
-        title: Text("Connexion"),
+        title: const Text("Mes conversations"),
         centerTitle: true,
       ),
       body: bodyPage(),
     );
   }
-  Widget bodyPage(){
+
+  Widget bodyPage() {
     return StreamBuilder<QuerySnapshot>(
-      stream: FirestoreHelper().fire_user.snapshots(),
-        builder: (context, snapshot){
-        if(!snapshot.hasData){
-          return CircularProgressIndicator();
-        }
-        else
-          {
+        stream: FirestoreHelper().fire_user.snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return CircularProgressIndicator();
+          } else {
             List documents = snapshot.data!.docs;
             return ListView.builder(
-              itemCount: documents.length,
-                itemBuilder: (context,index){
-                Utilisateur user = Utilisateur(documents[index]);
+                itemCount: documents.length,
+                itemBuilder: (context, index) {
+                  Utilisateur user = Utilisateur(documents[index]);
                   return ListTile(
-                    onTap: (){
-                      Navigator.push(context, MaterialPageRoute(
-                          builder: (context){
-                            return detail(user: user);
-                          }
-                      ));
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return detail(user: user);
+                      }));
                     },
-                    title:Text("${user.prenom}"),
-                    trailing: IconButton(onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(
-                          builder: (context){
+                    title: Text("${user.prenom}"),
+                    trailing: IconButton(
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
                             return messenger(user: user);
-                          }
-                      ));
-                    },
+                          }));
+                        },
                         icon: Icon(Icons.message_outlined)),
                   );
-
-
-
-                }
-            );
+                });
           }
-        }
-    );
+        });
   }
 }
--- lib/messenger.dart
+++ lib/messenger.dart
@@ -34,7 +34,11 @@ class messengerState extends State<messenger> {
           children: [
             Container(
               margin: EdgeInsets.all(10),
-              padding: EdgeInsets.only(left: 10.0, top: 10, bottom: 10, right: MediaQuery.of(context).size.width/5),
+              padding: EdgeInsets.only(
+                  left: 10.0,
+                  top: 10,
+                  bottom: 10,
+                  right: MediaQuery.of(context).size.width / 5),
               width: MediaQuery.of(context).size.width,
               child: Container(
                 decoration: BoxDecoration(
@@ -46,7 +50,11 @@ class messengerState extends State<messenger> {
             ),
             Container(
               margin: EdgeInsets.all(10),
-              padding: EdgeInsets.only(left: MediaQuery.of(context).size.width/5, top: 10, bottom: 10, right: 10.0),
+              padding: EdgeInsets.only(
+                  left: MediaQuery.of(context).size.width / 5,
+                  top: 10,
+                  bottom: 10,
+                  right: 10.0),
               width: MediaQuery.of(context).size.width,
               child: Container(
                 decoration: BoxDecoration(
@@ -58,43 +66,61 @@ class messengerState extends State<messenger> {
             ),
             Container(
               margin: EdgeInsets.all(10),
-              padding: EdgeInsets.only(left: MediaQuery.of(context).size.width/5, top: 10, bottom: 10, right: 10.0),
+              padding: EdgeInsets.only(
+                  left: MediaQuery.of(context).size.width / 5,
+                  top: 10,
+                  bottom: 10,
+                  right: 10.0),
               width: MediaQuery.of(context).size.width,
               child: Container(
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.black),
                 ),
-                child: Text("Comment vont les enfants?\nToujours en maternelle?"),
+                child:
+                    Text("Comment vont les enfants?\nToujours en maternelle?"),
                 padding: EdgeInsets.all(10),
               ),
             ),
             Container(
               margin: EdgeInsets.all(10),
-              padding: EdgeInsets.only(left: 10.0, top: 10, bottom: 10, right: MediaQuery.of(context).size.width/5),
+              padding: EdgeInsets.only(
+                  left: 10.0,
+                  top: 10,
+                  bottom: 10,
+                  right: MediaQuery.of(context).size.width / 5),
               width: MediaQuery.of(context).size.width,
               child: Container(
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.black),
                 ),
-                child: Text("Ils sont mort.\nDans un accident de voiture.\nHeureusement ils n'ont pas souffert.\nIls sont mort sur le coup"),
+                child: Text(
+                    "Ils sont mort.\nDans un accident de voiture.\nHeureusement ils n'ont pas souffert.\nIls sont mort sur le coup"),
                 padding: EdgeInsets.all(10),
               ),
             ),
             Container(
               margin: EdgeInsets.all(10),
-              padding: EdgeInsets.only(left: MediaQuery.of(context).size.width/5, top: 10, bottom: 10, right: 10.0),
+              padding: EdgeInsets.only(
+                  left: MediaQuery.of(context).size.width / 5,
+                  top: 10,
+                  bottom: 10,
+                  right: 10.0),
               width: MediaQuery.of(context).size.width,
               child: Container(
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.black),
                 ),
                 child: Text("Ah zut"),
-                padding: EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
               ),
             ),
             Container(
-              margin: EdgeInsets.all(10),
-              padding: EdgeInsets.only(left: 10.0, top: 10, bottom: 10, right: MediaQuery.of(context).size.width/5),
+              margin: const EdgeInsets.all(10),
+              padding: EdgeInsets.only(
+                  left: 10.0,
+                  top: 10,
+                  bottom: 10,
+                  right: MediaQuery.of(context).size.width / 5),
               height: 150,
               width: MediaQuery.of(context).size.width,
               child: Container(
@@ -106,8 +132,12 @@ class messengerState extends State<messenger> {
               ),
             ),
             Container(
-              margin: EdgeInsets.all(10),
-              padding: EdgeInsets.only(left: 10.0, top: 10, bottom: 10, right: MediaQuery.of(context).size.width/5),
+              margin: const EdgeInsets.all(10),
+              padding: EdgeInsets.only(
+                  left: 10.0,
+                  top: 10,
+                  bottom: 10,
+                  right: MediaQuery.of(context).size.width / 5),
               height: 150,
               width: MediaQuery.of(context).size.width,
               child: Container(
@@ -119,8 +149,12 @@ class messengerState extends State<messenger> {
               ),
             ),
             Container(
-              margin: EdgeInsets.all(10),
-              padding: EdgeInsets.only(left: MediaQuery.of(context).size.width/5, top: 10, bottom: 10, right: 10.0),
+              margin: const EdgeInsets.all(10),
+              padding: EdgeInsets.only(
+                  left: MediaQuery.of(context).size.width / 5,
+                  top: 10,
+                  bottom: 10,
+                  right: 10.0),
               height: 150,
               width: MediaQuery.of(context).size.width,
               child: Container(
@@ -132,8 +166,12 @@ class messengerState extends State<messenger> {
               ),
             ),
             Container(
-              margin: EdgeInsets.all(10),
-              padding: EdgeInsets.only(left: 10.0, top: 10, bottom: 10, right: MediaQuery.of(context).size.width/5),
+              margin: const EdgeInsets.all(10),
+              padding: EdgeInsets.only(
+                  left: 10.0,
+                  top: 10,
+                  bottom: 10,
+                  right: MediaQuery.of(context).size.width / 5),
               height: 150,
               width: MediaQuery.of(context).size.width,
               child: Container(
@@ -146,17 +184,53 @@ class messengerState extends State<messenger> {
             ),
             SizedBox(
               width: MediaQuery.of(context).size.width,
-              height: 175.0,
+              height: 100.0,
             )
           ],
         ),
         Positioned(
           left: 0,
           bottom: 0,
-          child: Container(
-            height: 150,
-            width: MediaQuery.of(context).size.width,
-            color: Colors.green,
+          child: Center(
+            child: Container(
+                height: 80,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  color: Colors.green,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Flexible(
+                      child: TextField(
+                        decoration: InputDecoration(
+                            hintText: "Entrez votre message...",
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            )),
+                      ),
+                    ),
+                    const SizedBox(width: 20), // give it width
+                    InkWell(
+                      onTap: () {
+                        print("Message envoyé");
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.only(right: 10),
+                        height: 50,
+                        width: 50,
+                        child: const Center(
+                          child: Icon(
+                            Icons.send,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )),
           ),
         )
       ],
